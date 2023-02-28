8bd809e017699c946f319d0adce5b0cb08d05fc5
Reymon <rahidalgob@uce.edu.ec>
2023-02-27 19:47:53 -0500

    Paralelo

---
+++ src/main/java/com/example/demo/modelo/Estudiante.java
@@ -0,0 +1,56 @@
+package com.example.demo.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="estudiante")
+public class Estudiante {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_estu")
+	@SequenceGenerator(name = "seq_estu", sequenceName = "seq_estu", allocationSize = 1)	
+	@Column(name = "estu_id")
+	private Integer id;
+	
+	
+	@Column(name="estu_nombre_completo")
+	private String nombreCompleto;
+	
+	@Column(name="estu_edad")
+	private Integer edad;
+
+	
+	//Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/paralelo/MainSecuencialStream.java
@@ -0,0 +1,49 @@
+package com.example.demo.paralelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class MainSecuencialStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+List<Integer> idEstudiantes = new ArrayList<>();
+		
+		for(int i=0; i<10; i++) {
+			idEstudiantes.add(i);
+		}
+		idEstudiantes.forEach(numero -> System.out.println("Estudiante:"+numero)); //Metodo HighOrder
+		//EL LOG.info es para ver la la hora y fecha exacta de esa ejecución
+		
+		//Luego, procesamos el estudiante
+		long tiempoInicial = System.currentTimeMillis();
+		List<String> listaFinal = idEstudiantes.stream().map(id->procesarIdEstudiante(id)).collect(Collectors.toList());
+
+		long tiempoFinal = System.currentTimeMillis();
+		//Vamos a ver el tiempo que tardó la ejecución
+		long variacionTiempo = (tiempoFinal - tiempoInicial)/1000;
+		listaFinal.forEach(cadena -> System.out.println(cadena));
+		System.out.println("Tiempo de ejecución: "+variacionTiempo+" segundos");
+	}
+
+	public static String procesarIdEstudiante(Integer idEstudiante) {
+		//Vamos a convertir los enteros a Strings
+		String idConvertido = "E"+idEstudiante.toString();
+		//Luego, insertamos en la base
+		System.out.println("Insertando Estudiante: "+idConvertido+" con el hilo: "+  Thread.currentThread().getName());
+		//Vamos a decir que se tarde 1 segundo--> Simulamos con TimeUnit
+		try {
+			TimeUnit.SECONDS.sleep(1);
+			
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+			
+		}
+		return idConvertido;
+		
+	}
+
+}
